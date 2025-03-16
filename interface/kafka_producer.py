b764ed2d807be462b4bc9a89db0d5b7c8de6e961
dwkim <99duuk@gmail.com>
2025-03-16 20:55:48 +0900

    feat: 분류된 tags kafka 메시지로 es 저장 구현

---
+++ interface/kafka_producer.py
@@ -0,0 +1,63 @@
+"""
+Kafka로 메시지 발행 기능 담당 모듈
+태깅된 이미지 정보를 Kafka 토픽으로 전송
+"""
+
+from kafka import KafkaProducer # Kafka 프로튜서 클래스
+import json
+
+from utils.log_util import ProgressLogger
+
+
+class TagProducer:
+    """
+    Kafka로 태그 데이터 발행하는 클래스
+    """
+    def __init__(self, bootstrap_servers, topic_name):
+        """
+        생성자: Kafka 프로듀서 초기화
+        bootstrap_servers (list): Kafka 서버 주소 목록
+        topic_name (str): 토픽 이름
+        """
+        # Kafka 프로듀서 객체 생성
+        # value_serializer: Python 객체를 바이트로 직렬화하는 함수 지정 (JSON 형식)
+        self.producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda x: json.dumps(x).encode('utf-8') # Python 객체 -> JSON -> UTF-8
+        )
+        self.topic_name = topic_name
+        ProgressLogger.log_step("Kafka Producer 초기화됨")
+
+
+    def _get_timestamp(self):
+        """ 현재 시간을 ISO 형식 문자열로 반환 """
+        from datetime import datetime
+        return datetime.now().isoformat() # ex: 2025-03-10T15:30:45.123456
+
+    def send_tag_data(self, file_path, tags, primary_tag):
+        """
+        태그 데이터를 kafka 토픽으로 전송
+        file_path (str): 이동된 파일 경로
+        tags (list): 나머지 태그 목록
+        primary_tag (str): 대분류 태그
+        """
+        try:
+            # 전송할 메시지 구조
+            message = {
+                "file_path": file_path,
+                "primary_tag": primary_tag,
+                "tags" : tags,
+                "timestamp": self._get_timestamp(),
+            }
+            # Kafka 토픽으로 메시지 발행
+            self.producer.send(self.topic_name, value=message)
+            self.producer.flush()   # 즉시 전송 보장 (비동기 처리 시 제거 가능)
+            ProgressLogger.log_step(f"Sent to Kafka topic: '{self.topic_name}' : {message}")
+        except Exception as e:
+            ProgressLogger.log_error(f"Error sending to Kafka: {e}")
+
+
+    def close(self):
+        """ Kafka Producer 종료 """
+        self.producer.close()
+        ProgressLogger.log_step("Kafka Producer closed")
--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 어플리케이션 시작점
 각 컴포넌트를 초기화하고 실행 흐름 제어
 """
-
+import time
 from interface.directory_watcher import DirectoryWatcher
 from interface.kafka_producer import TagProducer
 from models.model_loader import ModelLoader
 from core.tagger import ImageTagger
 from core.file_manager import FileManager
 from config import Config
+from utils.log_util import ProgressLogger
 
 # 전역 변수 선언
 tagger = None
@@ -22,28 +23,28 @@ def process_image(image_path):
 
     image_path (str): 처리할 이미지 파일 경로
     """
-    print(f"Processing: {image_path}")
+    ProgressLogger.log_step(f"Processing: {image_path}")
     try:
         # 1. 이미지 분석
         primary_tag, tags = tagger.analyze_image(image_path)
 
         # 태그가 없으면 처리 중단
         if not primary_tag:
-            print(f"No recognized objects in {image_path}, skipping...")
+            ProgressLogger.log_step(f"No recognized objects in {image_path}, skipping...")
             return
 
-        print(f"Image {image_path} classified - Primary tag: {primary_tag}, Tags: {tags}")
+        ProgressLogger.log_step(f"Image {image_path} classified - Primary tag: {primary_tag}, Tags: {tags}")
 
 
         # 2. 파일 이동
         new_path = file_manager.move_file(image_path, primary_tag)
-        print(f"Moved to: {new_path}")
+        ProgressLogger.log_step(f"Moved to: {new_path}")
 
         # 3. Kafka로 태그 정보 전송
-        # kafka_producer.send_tag_data(new_path, tags, primary_tag)
+        kafka_producer.send_tag_data(new_path, tags, primary_tag)
 
     except Exception as e:
-        print(f"이미지 처리 오류: {e}")
+        ProgressLogger.log_error(f"이미지 처리 오류: {e}")
 
 
 
@@ -56,39 +57,31 @@ def main():
     # 설정 로드
     config = Config()
     try:
-        # 모델 로더 초기화
-        model_loader = ModelLoader(config.model_path)
-
-        # 전역 변수로 선언한 컴포넌트들 초기화
-        global  tagger, file_manager, kafka_producer
-
-        # 태거 초기화 (모델 로더 및 태그 매핑 전달)
-        tagger = ImageTagger(model_loader, config.tag_mapping, config.color_ranges)
-        # 파일 관리자 초기화 (출력 디렉터리 전달)
-        file_manager = FileManager(config.output_dir)
-        # Kafka 프로듀서 초기화 (서버 주소 및 토픽 이름 전달)
-        kafka_producer = TagProducer(config.kafka_bootstrap_servers, config.kafka_topic_name)
-
+        model_loader = ModelLoader(config.model_path)                                           # 모델 로더 초기화
+        global  tagger, file_manager, kafka_producer                                            # 전역 변수로 선언한 컴포넌트들 초기화
+        tagger = ImageTagger(model_loader, config.tag_mapping, config.color_ranges)             # 태거 초기화 (모델 로더 및 태그 매핑 전달)
+        file_manager = FileManager(config.output_dir)                                           # 파일 관리자 초기화 (출력 디렉터리 전달)
+        kafka_producer = TagProducer(config.kafka_bootstrap_servers, config.kafka_topic_name)   # Kafka 프로듀서 초기화 (서버 주소 및 토픽 이름 전달)
 
         # 디렉터리 감시 시작
         watcher = DirectoryWatcher(config.input_dir)
         # process_image 함수를 이벤트 콜백으로 등록
         watcher.start(process_image)
-        print("디렉터리 감시 시작")
+        ProgressLogger.log_step("디렉터리 감시 시작")
     except Exception as e:
-        print(f"초기화 오류: {e}")
+        ProgressLogger.log_error(f"초기화 오류: {e}")
         return
 
     try:
         # 메인 스레드 계속 실행 유지
-        import time
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         # Ctrl+C로 프로그램 종료 시 디렉터리 감시도 중지
-        print("프로그램 종료 중...")
+        ProgressLogger.log_step("프로그램 종료 중...")
         watcher.stop()
-        print("디렉터리 감시 중지됨.")
+        kafka_producer.close()  # kafka Producer 종료
+        ProgressLogger.log_step("디렉터리 감시 중지됨.")
 
 
 if __name__ == "__main__":
